f148a4f8d9cb0608c77b01e1e0dda45def924829
chandlernolan895 <115596864+chandlernolan895@users.noreply.github.com>
2022-12-27 18:16:13 -0500

    Update submission.js
    findFrequency problem

--- submission.js
+++ submission.js
@@ -4,7 +4,36 @@ const findSum = function(array) {
 };
 
 const findFrequency = function(array) {
-  // your code here - don't forget to return an object!
+    let obj = {}
+    //create an object that tracks how many instances there are of each letter
+    for (const elem in arr){
+      if (!(obj[String(arr[elem])])){
+        obj[String(arr[elem])] = 1
+      } else{
+        obj[String(arr[elem])] = obj[String(arr[elem])] + 1
+      }
+   console.log(obj)
+    }
+    let most = obj[arr[0]];
+    let least = obj[arr[0]];
+    for (const i in arr){
+      if(obj[arr[i]] > most){
+        most = obj[arr[i]]
+      }
+      if(obj[arr[i]] < least){
+        least = obj[arr[i]]
+      }
+    }
+    let new_object = {most: 0, least:0}
+    for (const elem in obj){
+      if(obj[elem] == most){
+        new_object.most = elem
+      }
+      if(obj[elem] == least){
+        new_object.least = elem
+      }
+    }
+  return new_object
 };
 
 const isPalindrome = function(str) {
